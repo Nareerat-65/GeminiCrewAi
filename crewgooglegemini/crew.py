9852211fd60f98fcfc85bf5e7b881912d0dd2448
Nareerat-65 <nareeerat28012547@gmail.com>
2024-12-25 19:47:36 +0700

    final

--- crewgooglegemini/crew.py
+++ crewgooglegemini/crew.py
@@ -1,24 +1,25 @@
-from crewai import Crew,Process
-from tasks import research_task,write_task, package_planner_task, summarize_travel_task
-from agents import researcher,writer,package_planner,summarize
+from crewai import Crew, Process
+from tasks import research_task, write_task, package_planner_task, summarize_travel_task
+from agents import researcher, writer, package_planner, summarize
 
-## Forming the tech focused crew with some enhanced configuration
-crew=Crew(
-    agents=[researcher,writer,package_planner,summarize],
-    tasks=[research_task,write_task, package_planner_task, summarize_travel_task],
+# Forming the tech focused crew with some enhanced configuration
+crew = Crew(
+    agents=[researcher, writer, package_planner, summarize],
+    tasks=[research_task, write_task, package_planner_task, summarize_travel_task],
     process=Process.sequential,
-
 )
 
-## Input from the user
-day_input = input("Please enter the day: ")
-people_input = input("Please enter the number of people: ")
-loaction_input = input("จังหวัดที่ต้องการไป (น่าน, เชียงใหม่, เชียงราย, แม่ฮ่องสอน, เพชรบูรณ์): ")
-interest_input = input("Please enter the interest (ธรรมชาติ, วัด, etc.): ")
+# Prompting the user for multiple inputs
+day_input = input("ไปกี่วัน : ")
+people_input = input("ไปกี่คน : ")
+location_input = input("จังหวัดที่ต้องการไป (น่าน, เชียงใหม่, เชียงราย, แม่ฮ่องสอน, เพชรบูรณ์) : ")
+interest_input = input("ความสนใจ (ธรรมชาติ, วัด, etc.) : ")
 
-## formatting the expected output
-package_planner_task.expected_output = package_planner_task.expected_output.format(day_input=day_input)
+# Update the expected_output for tasks
+research_task.expected_output = research_task.expected_output.format(day_input=day_input, people_input=people_input, location_input=location_input, interest_input=interest_input)
+package_planner_task.expected_output = package_planner_task.expected_output.format(day_input=day_input, location_input=location_input)
+summarize_travel_task.expected_output = summarize_travel_task.expected_output.format(day_input=day_input, people_input=people_input, location_input=location_input, interest_input=interest_input)
 
-## Running the crew
-result=crew.kickoff(inputs={"Day" : day_input, "Location" : loaction_input, "People" : people_input, "Interest" : interest_input})
+# Starting the task execution process with enhanced feedback
+result = crew.kickoff(inputs={"Day": day_input, "People": people_input, "Location": location_input, "Interest": interest_input})
 print(result)
